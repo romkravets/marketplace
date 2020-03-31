3e8ff4ad58689f44ee3f63bbfe78281d3ec8b793
romkravets <romann.kravets@gmail.com>
2020-03-31 16:59:11 +0300

    add

---
+++ src/containers/Auth/Login/Login.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+// const login = props => {
+
+// }
+
+// export default login;
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import axios from "../../axios-orders";
+import Prodacts from "../../components/Prodacts/Prodacts";
+
+const home = (props) => {
+  console.log(props);
+  const [prodactsState, setProdactsState] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://marketplace-91001.firebaseio.com/products.json")
+      .then((response) => {
+        const prodacts = response.data;
+        const updateProdacts = prodacts.map((prodact) => {
+          return {
+            ...prodact
+          };
+        });
+        setProdactsState(updateProdacts);
+        //console.log(updateProdacts);
+      });
+  }, []);
+
+  const isFavoriteHandler = () => {
+    // const disabledInfo = {
+    //    ...prodactsState
+    // };
+    // console.log(disabledInfo);
+    // for (let key in disabledInfo) {
+    //    console.log(disabledInfo[key].favorite);
+    // }
+  };
+
+  return (
+    <div>
+      <Prodacts prodacts={prodactsState} favoriteCliced={isFavoriteHandler} />
+    </div>
+  );
+};
+
+export default home;
