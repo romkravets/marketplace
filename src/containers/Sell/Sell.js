2013cdd95494eb11111c9b2f826ad5abc70e5cc4
romkravets <romann.kravets@gmail.com>
2020-04-03 20:03:39 +0300

    style sell

--- src/containers/Sell/Sell.js
+++ src/containers/Sell/Sell.js
@@ -4,44 +4,88 @@ import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
 import { updateObject, checkValidity } from "../../shared/utillity";
 
+import classes from "./Sell.css";
+
 class Sell extends Component {
   state = {
     controls: {
-      email: {
+      title: {
         elementType: "input",
         elementConfig: {
-          type: "email",
+          type: "text",
           placeholder: "Mail Address"
         },
         value: "",
         validation: {
-          required: true,
-          isEmail: true
+          required: true
+          //isEmail: true
         },
         valid: false,
         touched: false
       },
-      password: {
+      location: {
         elementType: "input",
         elementConfig: {
-          type: "password",
-          placeholder: "Password"
+          type: "text",
+          placeholder: "Location"
         },
         value: "",
         validation: {
-          required: true,
-          minLength: 6
+          required: true
+          //minLength: 6
+        },
+        valid: false,
+        touched: false
+      },
+      description: {
+        elementType: "textarea",
+        elementConfig: {
+          type: "text",
+          placeholder: "For example: Iron man suit"
+        },
+        value: "",
+        validation: {
+          required: true
+          //minLength: 6
+        },
+        valid: false,
+        touched: false
+      },
+      image: {
+        elementType: "input",
+        elementConfig: {
+          type: "file",
+          placeholder: ""
+        },
+        value: "",
+        validation: {
+          required: true
+          //minLength: 6
+        },
+        valid: false,
+        touched: false
+      },
+      price: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Price"
+        },
+        value: "",
+        validation: {
+          required: true
+          //minLength: 6
         },
         valid: false,
         touched: false
       }
     },
-    title: "",
-    location: "",
-    description: "",
+    // title: "",
+    // location: "",
+    // description: "",
     image: "https://i.ibb.co/HBVnDZm/Mask.png",
-    author: "Max",
-    price: "",
+    // author: "Max",
+    // price: "",
     favorite: false
   };
 
@@ -60,20 +104,42 @@ class Sell extends Component {
     this.setState({ controls: updatedControls });
   };
 
-  postDataHandler = () => {
+  submitHandler = (event) => {
+    event.preventDefault();
+    // this.props.onAuth(
+    //   this.state.controls.email.value,
+    //   this.state.controls.password.value,
+    //   this.state.isSignup
+    // );
     const data = {
-      title: this.state.title,
-      location: this.state.location,
-      description: this.state.description,
+      title: this.state.controls.title.value,
+      location: this.state.controls.location.value,
+      description: this.state.controls.description.value,
       image: this.state.image,
       author: this.state.author,
-      price: this.state.price,
+      price: this.state.controls.price.value,
       favorite: this.state.favorite
     };
     axios.post("/product.json", data).then((response) => {
       console.log(response);
     });
+    this.props.history.push("/");
   };
+
+  // postDataHandler = () => {
+  //   const data = {
+  //     title: this.state.title,
+  //     location: this.state.location,
+  //     description: this.state.description,
+  //     image: this.state.image,
+  //     author: this.state.author,
+  //     price: this.state.price,
+  //     favorite: this.state.favorite
+  //   };
+  //   axios.post("/product.json", data).then((response) => {
+  //     console.log(response);
+  //   });
+  // };
 
   render() {
     const formElmentsArray = [];
@@ -98,47 +164,12 @@ class Sell extends Component {
       />
     ));
     return (
-      <div className="NewPost">
+      <div className={classes.Sell}>
         <h2>Add product</h2>
-        <label>Title</label>
-        <input
-          type="text"
-          value={this.state.title}
-          onChange={(event) => this.setState({ title: event.target.value })}
-        />
-        <label>Location</label>
-        <input
-          type="text"
-          value={this.state.location}
-          onChange={(event) => this.setState({ location: event.target.value })}
-        />
-        <label>Description</label>
-        <textarea
-          rows="4"
-          value={this.state.content}
-          onChange={(event) =>
-            this.setState({ description: event.target.value })
-          }
-        />
-        <label>Author</label>
-        <select
-          value={this.state.author}
-          onChange={(event) => this.setState({ author: event.target.value })}
-        >
-          <option value="Max">Max</option>
-          <option value="Manu">Manu</option>
-        </select>
-        <label>Price</label>
-        <input
-          type="text"
-          value={this.state.price}
-          onChange={(event) => this.setState({ price: event.target.value })}
-        />
-        <Button btnType="Success" onClick={this.postDataHandler}>
-          SUBMIT
-        </Button>
-
-        <form>{form}</form>
+        <form onSubmit={this.submitHandler}>
+          {form}
+          <Button btnType="Success">SUBMIT</Button>
+        </form>
       </div>
     );
   }
